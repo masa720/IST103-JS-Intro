064c83064d05d87d8e97a0f5a90427559a36b699
Masaki <>
2025-03-10 20:22:43 -0700

    Masaki-ClassActivity2-10March2025#2

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const lastName = "Otani";
 const name = firstName + lastName;
 const age = 30;
 const isMarried = true;
+
+console.log("Person name is" + name);
+console.log("Person age is" + age);
+console.log("Is person married?" + isMarried);
